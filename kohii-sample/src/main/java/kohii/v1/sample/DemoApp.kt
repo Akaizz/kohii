77cb76916f1719f23b9ce0dc73c53bd037aae8c5
akaizz <buiquangviet@gapo.com.vn>
2025-08-04 10:31:01 +0700

    Remove unused `Manilo` instance in `DemoApp`.

--- kohii-sample/src/main/java/kohii/v1/sample/DemoApp.kt
+++ kohii-sample/src/main/java/kohii/v1/sample/DemoApp.kt
@@ -26,7 +26,6 @@ import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
 import com.squareup.moshi.Types
-import kohii.v1.ads.Manilo
 import kohii.v1.sample.data.Item
 import kohii.v1.sample.data.Video
 import kohii.v1.sample.ui.ads.AdsContainerFragment
@@ -236,10 +235,6 @@ class DemoApp : Application() {
         ),
       )
       .toList()
-  }
-
-  val manilo: Manilo by lazy(NONE) {
-    Manilo[this]
   }
 
   override fun onCreate() {
